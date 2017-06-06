8fa0f6fa110ddb82c2ce48786e316210ab022bb6
pkindalov <panayotkindalov@gmail.com>
2017-06-06 17:34:12 +0300

    Changed name of database to carsRental

--- server/config/settings.js
+++ server/config/settings.js
@@ -5,7 +5,7 @@ let rootPath = path.normalize(path.join(__dirname, '/../../'))
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: 'mongodb://localhost:27017/generictemplate',
+    db: 'mongodb://localhost:27017/carsRental',
     port: 1337
   },
   staging: {
