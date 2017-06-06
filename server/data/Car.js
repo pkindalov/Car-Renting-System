36f880975edb8353ecf4f251c8d750bdf45dc7f3
pkindalov <panayotkindalov@gmail.com>
2017-06-06 22:43:21 +0300

    Created model car in data directory

---
+++ server/data/Car.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+let carSchema = new mongoose.Schema({
+  make: {type: String, required: true},
+  model: {type: String, required: true},
+  year: {type: Number, required: true},
+  pricePerDay: {type: Number, required: true},
+  power: {type: Number},
+  image: {type: String, required: true}
+})
+
+let Car = mongoose.model('Car', carSchema)
+
+module.exports = Car
